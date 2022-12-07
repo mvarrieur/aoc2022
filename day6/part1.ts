4ec0c66e701095e59f2d80939143f996d989b3df
Michael Varrieur <mvarrieur@users.noreply.github.com>
2022-12-07 08:31:57 -0500

    Day 6

---
+++ day6/part1.ts
@@ -0,0 +1,26 @@
+import unique from "../utils/uniqueInArray.ts";
+
+const sample = Deno.readTextFileSync("./sample.txt");
+const input = Deno.readTextFileSync("./input.txt");
+
+const signals = input;
+
+let solution = -1;
+
+const signal = signals.split("").reduce((acc, curr, idx, arr) => {
+  if (acc.length < 4) {
+    acc.push(curr);
+  } else {
+    acc.shift();
+    acc.push(curr);
+  }
+
+  if (acc.filter(unique).length === 4) {
+    solution = idx + 1;
+    arr.splice(0); // Mutate the original array to empty as a way to break out of this reduce early
+  }
+
+  return acc;
+}, [] as string[]);
+
+console.log({ signal, solution });
